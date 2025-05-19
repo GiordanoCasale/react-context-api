bfb5f9ec9f13d153bbba622c1b733b8dedddeb1e
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-19 14:50:07 +0200

    [UPD]implement routing and add About, Posts, and Homepage components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Homepage from './components/pages/Homepage'
+import About from './components/pages/About'
+import Posts from './components/pages/Posts'
 
 function App() {
 
 
   return (
-    <div>
-      <h1>hello!</h1>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/posts' element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Postpage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Postpage = () => {
+    return (
+        <div>
+            <h1>Ciao!</h1>
+        </div>
+    )
+}
+
+export default Postpage
